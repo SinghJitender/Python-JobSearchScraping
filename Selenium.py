314fbe035d9a212214ef791d41e1fedb4dd785c0
Jitender <jitu.singh95@gmail.com>
2020-11-20 23:27:18 +0530

    InitialSetup

--- Selenium.py
+++ Selenium.py
@@ -2,6 +2,9 @@ from selenium import webdriver
 
 searchBox = "//div[@class='inpWrap']//input[@type='text' and @name='keyword']"
 searchButton = "//div[@class='search-btn']//button"
+searchedJobTitle = "//div[@class='list']//a[contains(@class,'fw')]"
+
+#What Job to search for
 serachJob = "Developer"
 
 #filters
@@ -11,8 +14,23 @@ experience = ""
 postedby = ""
 
 driver = webdriver.Chrome()
+
+#Script only works on naukri.com
 driver.get("https://www.naukri.com")
+
 driver.find_element_by_xpath(searchBox).send_keys(serachJob)
 driver.find_element_by_xpath(searchButton).click()
-print(driver.page_source)
+driver.implicitly_wait(5)
+list = driver.find_elements_by_xpath(searchedJobTitle)
+print(len(list))
+s = list[0].get_attribute("href")
+print(s)
+import requests
+r = requests.get(s)
+print(r)
+#print(driver.page_source)
+from bs4 import BeautifulSoup
+c = BeautifulSoup(r.text,features="html.parser")
+print(c)
+#print(r.text)
 #driver.close()
