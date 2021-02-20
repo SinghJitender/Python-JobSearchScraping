d6d756a4961cf3e528f27ca431bb1b69be86e4a9
Jitender <jitu.singh95@gmail.com>
2021-02-20 22:31:08 +0530

    Hackerrank Question - theFullCountingSort.py.py Done

--- Hackerrank/theFullCountingSort.py
+++ Hackerrank/theFullCountingSort.py
@@ -10,17 +10,15 @@ import sys
 # Complete the countSort function below.
 def countSort(arr):
     x = dict()
-    y = set()
-    for i in range(len(arr)/2):
+    for i in range(int(len(arr) / 2)):
         arr[i][1] = "-"
     for i in arr:
-        if i[0] in x:
-            x[i[0]].append(i[1])
+        if int(i[0]) in x:
+            x[int(i[0])].append(i[1])
         else:
-            x[i[0]] = [i[1]]
-            y.add(i[0])
+            x[int(i[0])] = [i[1], ]
 
-    for i in sorted(y):
+    for i in sorted(x):
         for j in x[i]:
             print(j, end=" ")
 
