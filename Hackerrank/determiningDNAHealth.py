5435116af6596a3ee7045d846d1c358b341b74dd
Jitender <jitu.singh95@gmail.com>
2021-02-23 21:35:48 +0530

    Hackerrank determiningDNAHealth.py - Completed but not optimized

--- Hackerrank/determiningDNAHealth.py
+++ Hackerrank/determiningDNAHealth.py
@@ -1,11 +1,12 @@
-# https://www.hackerrank.com/challenges/determining-dna-health/problem
-# !/bin/python3
+#!/bin/python3
 
 import math
 import os
 import random
 import re
 import sys
+
+
 
 if __name__ == '__main__':
     n = int(input())
@@ -29,29 +30,32 @@ if __name__ == '__main__':
 
         healthy = dict()
         for x in range(first, last + 1):
-            if len(genes[x]) in healthy:
-                if healthy[len(genes[x])][genes[x]] in healthy[len(genes[x])]:
-                    healthy[len(genes[x])][genes[x]] = healthy[len(genes[x])][genes[x]] + health[x]
+            key = genes[x]
+            lenKey = len(key)
+            if lenKey in healthy:
+                if key in healthy[lenKey]:
+                    healthy[lenKey][key] = healthy[lenKey][key] + health[x]
                 else:
-                    healthy[len(genes[x])].update({genes[x]: health[x]})
-
+                    healthy[lenKey].update({key: health[x]})
             else:
-                healthy[len(genes[x])] = {genes[x]: health[x]}
-        print(healthy)
+                healthy[lenKey] = {genes[x]: health[x]}
+        #print(healthy)
+        #print(healthy)
         temp = 0;
-        # print(d)
+        #print(d)
         for i in healthy:
-            # print(i)
-            # print(list(range(0,len(d)-len(i)+1)))
-            for x in range(0, len(d) - len(i) + 1):
-                # print("%s == %s"%(d[int(x):int(x)+int(len(i))],i))
-                if d[int(x):int(x) + int(len(i))] == i:
-                    temp += healthy[i]
+            #print(i)
+            #print(list(range(0,len(d)-len(i)+1)))
+            for x in range(0,len(d)-i+1):
+                #print("%s == %s"%(d[int(x):int(x)+int(len(i))],i))
+                spl = d[x:x+i]
+                if spl in healthy[i]:
+                    temp += healthy[i][spl]
 
-        # print("Total Health - %s"%(temp))
-        if (temp > max):
+        #print("Total Health - %s"%(temp))
+        if( temp>max):
             max = temp
-        if (temp < min):
+        if(temp<min):
             min = temp
 
-    print("%d %d" % (min, max))
+    print("%d %d"%(min,max))
